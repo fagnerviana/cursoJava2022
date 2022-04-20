9f7edf5f70f4af2526f348ad83e163f326ac1c94
fagnerviana <fagsantiago@gmail.com>
2022-04-20 11:10:26 -0300

    Trabalhando com If e Else

---
+++ src/estruturasDeControle/IfElseALinhado.java
@@ -0,0 +1,20 @@
+package estruturasDeControle;
+
+public class IfElseALinhado {
+
+	public static void main(String[] args) {
+		//criando uma nova variavel
+		int hora =17;
+		
+		//aninhamento de ifs e elses
+		if(hora < 12) {
+			System.out.println("Bom dia ! Como vocês estão ?");
+		}else if (hora < 18) {
+			System.out.println("Boa tarde ! Tudo bem ?");
+		}else {
+			System.out.println("Entendi ! è noite , então boa noite!");
+		}
+
+	}
+
+}
---
+++ src/estruturasDeControle/IfeElse.java
@@ -0,0 +1,19 @@
+package estruturasDeControle;
+
+public class IfeElse {
+
+	public static void main(String[] args) {
+		//desclarar uma variavel
+		int x = 10;
+		
+		//implementado a estrutura de decisão
+		if(x != 10) {
+			System.out.println("x é igual ao valor de 10");
+			
+		}else {
+			System.out.println("O valor de x é:" + x);
+		}
+	
+	}
+	
+}
